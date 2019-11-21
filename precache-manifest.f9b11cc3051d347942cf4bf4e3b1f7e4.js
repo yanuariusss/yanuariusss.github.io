22c229041eac5dbdee2d5522e997519f659e11b6
= <yanuariush@gmail.com>
2019-11-21 15:13:16 +0700

    UGD12_C_9454

---
+++ precache-manifest.f9b11cc3051d347942cf4bf4e3b1f7e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd0b41f3163d931d8c2",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "baf5b826c43e6b79bbbf",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "849941297da4c24901cd3f070ee06144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bd0b41f3163d931d8c2",
+    "url": "/js/about.25ede9d6.js"
+  },
+  {
+    "revision": "66cb033a7c3fac293ee5",
+    "url": "/js/app.b486ee38.js"
+  },
+  {
+    "revision": "baf5b826c43e6b79bbbf",
+    "url": "/js/chunk-vendors.ef92f471.js"
+  },
+  {
+    "revision": "96f8c373dcf5a3e55984675ee42ca8a6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
